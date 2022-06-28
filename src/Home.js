0ec3ce11075a96e8ee8a9fb0a64f65628407ffac
Puneet <puneet.poojary@thinkly.in>
2022-06-28 17:56:34 +0530

    first upload on github

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React,{useContext} from 'react'
+// import {AppContext} from "./context"
+import {useGlobalContext} from "./context" 
+import Movies from './Movies'
+import Search from './Search'
+
+
+const Home = () => {
+
+
+  return (
+  <>
+  
+
+<Search />
+<Movies/>
+
+
+  
+
+    </>
+  )
+}
+
+export default Home
