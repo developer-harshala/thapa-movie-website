a0e9bb55758ae58ee5ed44f12557a5dd461a7c52
Harshala Gawli <rinkusgawli@gmail.com>
2024-08-02 15:00:03 +0530

    Start again

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react'
-import { Route ,Routes} from 'react-router-dom'
-import Home from "./Home"
-import SingleMovie from "./SingleMovie"
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import SingleMovie from './SingleMovie'
 import Error from './Error'
 
 const App = () => {
   return (
     <>
-    
-
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='movie/:id' element={<SingleMovie/>}/>
-<Route path='*' element={<Error/>}/>
-  </Routes>
-
-
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='movie/:id' element={<SingleMovie />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </>
   )
 }
